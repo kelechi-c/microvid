d04110e4a45f173d2199a7875450abf6d6e7b683
Tensor <narutokelechi@gmail.com>
2024-12-14 11:42:58 +0100

    [update] rectified flow module

--- microdit/microdit.py
+++ microdit/microdit.py
@@ -1186,43 +1186,48 @@ class MicroDiT(nn.Module):
         return (images[-1] / config.vaescale_factor)
 
 
-class RectFlowWrapper:
+class RectFlowWrapper(nn.Module):
     def __init__(self, model, ln=True):
         self.model = model
         self.ln = ln
 
-    def forward(self, x, cond):
+    def forward(self, x, cond, mask):
         b = x.size(0)
+
         if self.ln:
             nt = torch.randn((b,)).to(x.device)
             t = torch.sigmoid(nt)
         else:
             t = torch.rand((b,)).to(x.device)
+
         texp = t.view([b, *([1] * len(x.shape[1:]))])
-        z1 = torch.randn_like(x)
-        zt = (1 - texp) * x + texp * z1
-        vtheta = self.model(zt, t, cond)
-        batchwise_mse = ((z1 - x - vtheta) ** 2).mean(dim=list(range(1, len(x.shape))))
-        tlist = batchwise_mse.detach().cpu().reshape(-1).tolist()
-        ttloss = [(tv, tloss) for tv, tloss in zip(t, tlist)]
-        return batchwise_mse.mean(), ttloss
+
+        z_noise = torch.randn_like(x)
+        z_noise_t = (1 - texp) * x + texp * z_noise
+
+        vtheta = self.model(z_noise_t, t, cond, mask)
+
+        batchwise_mse = ((z_noise - x - vtheta) ** 2).mean(dim=list(range(1, len(x.shape))))
+
+        return batchwise_mse.mean()
 
     @torch.no_grad()
-    def sample(self, z, cond, null_cond=None, sample_steps=50, cfg=2.0):
-        b = z.size(0)
+    def sample(self, z_noise, cond, null_cond=None, sample_steps=50, cfg=2.0):
+        b = z_noise.size(0)
         dt = 1.0 / sample_steps
-        dt = torch.tensor([dt] * b).to(z.device).view([b, *([1] * len(z.shape[1:]))])
-        images = [z]
+        dt = torch.tensor([dt] * b).to(z_noise.device).view([b, *([1] * len(z.shape[1:]))])
+        images = [z_noise]
+
         for i in range(sample_steps, 0, -1):
             t = i / sample_steps
-            t = torch.tensor([t] * b).to(z.device)
+            t = torch.tensor([t] * b).to(z_noise.device)
 
-            vc = self.model(z, t, cond)
+            vc = self.model(z_noise, t, cond)
             if null_cond is not None:
-                vu = self.model(z, t, null_cond)
+                vu = self.model(z_noise, t, null_cond)
                 vc = vu + cfg * (vc - vu)
 
-            z = z - dt * vc
-            images.append(z)
+            z_latent = z_latent - dt * vc
+            images.append(z_latent)
 
         return images
