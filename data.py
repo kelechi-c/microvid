070103b337892d4a7d48c7071ab1e5a271e2bbd6
Tensor <narutokelechi@gmail.com>
2024-12-31 03:32:43 +0100

    [data] dataloader file

--- data.py
+++ data.py
@@ -1,25 +1,29 @@
 from torch.utils.data import IterableDataset, DataLoader
 from datasets import load_dataset
 
+data_id = "tensorkelechi/tiny_webvid_latents"
 
 class Text2VideoDataset(IterableDataset):
-    def __init__(self, split=512):
+    def __init__(self, split=64):
         super().__init__()
         self.split = split
         self.dataset = load_dataset(
-            "tensorkelechi/tiny_webvid_latents",
+            data_id,
             streaming=True,
             split="train",
             trust_remote_code=True,
-        ).take(self.split)
+        ).take(
+            self.split
+        )  # Haven't preprocessed enough videos yet
 
     def __len__(self):
         return self.split
 
     def __iter__(self):
         for sample in self.dataset:
-            latents = sample["video_latents"]  # type: ignore
+            latents = torch.tensor(sample["hyv_latents"], dtype=torch.bfloat16)  # type: ignore
 
-            caption = sample["text_encoded"]  # type: ignore
+            caption = torch.tensor(sample["text_encoded"], dtype=torch.bfloat16)  # type: ignore
+            caption = caption[:, :10, :]
 
             yield latents, caption
